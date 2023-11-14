92a9c446926c8bbd0bfb25894e638c494673aa97
Harcihólapát <116527138+Harciholapat@users.noreply.github.com>
2023-11-14 11:57:39 +0100

    Add files via upload

---
+++ Cso.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Cso : Henger
+    {
+        private int falvastagsag;
+
+        public Cso(int magassag, int sugar, int szuletesSzamlalo, int falvastagsag) : base(magassag, sugar, szuletesSzamlalo)
+        {
+            this.falvastagsag = falvastagsag;
+        }
+
+        public int FalVastagsag
+        {
+            get => falvastagsag;
+        }
+
+        public void Terfogat()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Henger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Henger
+    {
+        private int magassag;
+        private int sugar;
+        private int szuletesSzamlalo;
+
+        public Henger(int magassag, int sugar, int szuletesSzamlalo)
+        {
+            this.magassag = magassag;
+            this.sugar = sugar;
+            this.szuletesSzamlalo = szuletesSzamlalo;
+        }
+
+        public int GetMagassag
+        {
+            get => magassag;
+        }
+
+        public int GetSugar
+        {
+            get => sugar;
+        }
+
+        public int SzuletesSzamlalo
+        {
+            get => szuletesSzamlalo;
+        }
+
+        public double Terfogat()
+        {
+            return Math.Pow(sugar, 2) * Math.PI * magassag;
+        }
+
+        public void ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
